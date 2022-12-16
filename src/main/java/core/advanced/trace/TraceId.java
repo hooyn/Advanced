c91f5deace3b434901a4edaf6fff0c85a5515eec
JungHoyun <hooyn_industry@naver.com>
2022-12-16 11:24:06 +0900

    feat: 로그 추적기 V1 - 프로토타입 개발

---
+++ src/main/java/core/advanced/trace/TraceId.java
@@ -0,0 +1,38 @@
+package core.advanced.trace;
+
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+public class TraceId {
+
+    private String id; // 트랜잭션 식별 아이디
+    private int level; // 트랜잭션 레벨
+
+    public TraceId() {
+        this.id = createId();
+        this.level = 0;
+    }
+
+    public TraceId(String id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
+    private String createId() {
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
+
+    public TraceId createNextId() {
+        return new TraceId(id, level + 1);
+    }
+
+    public TraceId createPreviousId() {
+        return new TraceId(id, level - 1);
+    }
+
+    public boolean isFirstLevel() {
+        return level == 0;
+    }
+}
---
+++ src/test/java/core/advanced/trace/adtrace/AdTraceV1Test.java
@@ -0,0 +1,23 @@
+package core.advanced.trace.adtrace;
+
+import core.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AdTraceV1Test {
+
+    @Test
+    void begin_end() {
+        AdTraceV1 trace = new AdTraceV1();
+        TraceStatus status = trace.begin("Advanced");
+        trace.end(status);
+    }
+
+    @Test
+    void begin_exception() {
+        AdTraceV1 trace = new AdTraceV1();
+        TraceStatus status = trace.begin("Advanced");
+        trace.exception(status, new IllegalStateException());
+    }
+}
